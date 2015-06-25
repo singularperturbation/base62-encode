c97b2351705be8b7b108fe3b84a4b103d595d10f
Sloane Simons <sloane.simmmons@rewardstyle.com>
2015-06-25 16:08:32 -0500

    Finished factory implementation
    Has user input the alphabet once, then generates encoder and decoder
    functions that use the alphabet.  The default base-62 alphabet is used
    if none is provided.  The advantage with having factory functions is
    that we only have to do the 'tableization' of the alphabet *once*, so
    subsequent decoding is very efficient.  Also, the encode/decode
    functions have a more natural API (string / int).

--- base62.nim
+++ base62.nim
@@ -8,44 +8,50 @@ const
   base: string = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
 type 
-  DecoderFunc* = proc(input: int):    string
-  EncoderFunc* = proc(input: string): int
+  DecoderFunc* = proc(input: string): int
+  EncoderFunc* = proc(input: int):    string
   FunctionPair* = tuple[encoder: EncoderFunc, decoder: DecoderFunc]
 
-proc encode*(input: int;
-            alphabet: string = base): string =
-  let
-    baseNum: int = len(alphabet)
-  var
-    remainder:  int = input mod baseNum
-    integerDiv: int = input div baseNum
-    encoded:    string = "" & alphabet[remainder]
+proc generateEncoder(alphabet: string = base): EncoderFunc =
+  let encode: EncoderFunc = (proc(input: int): string =
+      let
+        baseNum: int = len(alphabet)
+      var
+        remainder:  int = input mod baseNum
+        integerDiv: int = input div baseNum
+        encoded:    string = "" & alphabet[remainder]
 
-  while (integerDiv != 0):
-    remainder  = integerDiv mod baseNum
-    integerDiv = integerDiv div baseNum
-    encoded    = alphabet[remainder] & encoded
+      while (integerDiv != 0):
+        remainder  = integerDiv mod baseNum
+        integerDiv = integerDiv div baseNum
+        encoded    = alphabet[remainder] & encoded
 
-  result = encoded
+      result = encoded)
+  result = encode
 
-# Should return either decoded result or -1 for error
-proc decode*(input: string;
-            alphabet: string = base): int =
-  if not rs.isValidResult(input,alphabet):
-    return -1
-  var 
-      PositionMap  = rs.toCharMap(alphabet)
-      decoded: int = PositionMap[input[0]]
-  let
-    input_limit: int = len(input)
-    baseNum:     int = len(alphabet)
-  # For each letter of this, want to get position in input
-  for i in 1..<input_limit:
-    decoded = baseNum * decoded + PositionMap[input[i]]
-  return decoded
+proc generateDecoder(alphabet: string = base): DecoderFunc =
+  let PositionMap = rs.toCharMap(alphabet)
+  # Should return either decoded result or -1 for error
+  let decode: DecoderFunc = (proc(input: string): int =
+    if not rs.isValidResult(input,alphabet):
+      return -1
+    var 
+        decoded: int = PositionMap[input[0]]
+    let
+      input_limit: int = len(input)
+      baseNum:     int = len(alphabet)
+    # For each letter of this, want to get position in input
+    for i in 1..<input_limit:
+      decoded = baseNum * decoded + PositionMap[input[i]]
+    result = decoded)
+  result = decode
   
+proc GenerateFunctionPair*(alphabet: string = base): FunctionPair =
+  return (generateEncoder(alphabet),generateDecoder(alphabet))
+
 when isMainModule:
   let
+    (encode,decode) = GenerateFunctionPair()
     test_encoded:   string = "5e3T6"
     test_decoded:   int    = 77233220
     encoded_output: string = encode(test_decoded)
